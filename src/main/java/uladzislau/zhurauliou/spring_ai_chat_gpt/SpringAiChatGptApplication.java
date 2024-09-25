78dcc334ebdb4613847faeb268398518a583cce4
uzhuraul <uladzislau.zhurauliou@gmail.com>
2024-09-25 07:46:08 +0200

    Init commit

---
+++ src/main/java/uladzislau/zhurauliou/spring_ai_chat_gpt/SpringAiChatGptApplication.java
@@ -0,0 +1,13 @@
+package uladzislau.zhurauliou.spring_ai_chat_gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiChatGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uladzislau/zhurauliou/spring_ai_chat_gpt/SpringAiChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package uladzislau.zhurauliou.spring_ai_chat_gpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
